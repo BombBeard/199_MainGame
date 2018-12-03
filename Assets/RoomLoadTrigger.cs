26f336bf60171b7e37f84697a9d0abdf7c28f7db
Chris Moyer <everydaychris@gmail.com>
2018-12-03 02:04:35 -0800

    Room trigger load implemented
    New script which uses a collider to determine when to load a new scene. Checks to make sure a player is the object interacting.  Getting massive FPS-- may attempt porting to mac mini.

---
+++ Assets/RoomLoadTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(BoxCollider))]
+public class RoomLoadTrigger : MonoBehaviour {
+    BoxCollider trigger;
+    public string roomToLoad;
+
+
+	// Use this for initialization
+	void Start () {
+        trigger = GetComponent<BoxCollider>();
+        trigger.isTrigger = true;
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //load given scene
+        if (other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(roomToLoad);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
